a626ccc1228a99ad9c12ec443d4614fbf966d7cc
mori-dev <mori.dev.asdf@gmail.com>
2017-02-10 11:28:02 +0900

    fix: settings for webpack-dev-server v2.3

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -6,7 +6,6 @@ const baseConfig = require('./webpack.config.base.js');
 const config = merge.smart(baseConfig, {
     devtool: 'inline-source-map',
     devServer: {
-        outputPath: path.join(__dirname, './www/js'),
         contentBase: path.join(__dirname, './www'),
         port: 8090,
         host: '0.0.0.0',
